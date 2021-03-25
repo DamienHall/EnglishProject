5bb3e25a0393f8b4647959757a89814de4968223
LilZcrazyG <damien.z.hall@gmail.com>
2021-03-25 15:49:24 -0500

    FINALLY THE STICKY NAV BAR W O R K S CORRECTLY, 4 HOURSfasfdfasfdas

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 var content = document.getElementById("content");
-var originalMarginTop = content.style.marginTop;
+var originalOffsetTop =
+parseFloat(window.getComputedStyle(document.getElementById("content")).marginTop)
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
-
 window.onscroll = () => {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky");
-    content.style.marginTop = (content.style.marginTop+(navbar.clientHeight*2))+"px";
+    content.style.marginTop = (originalOffsetTop+parseFloat(window.getComputedStyle(navbar).height))+"px";
   } else {
     navbar.classList.remove("sticky");
-    content.style.marginTop = originalMarginTop;
+    content.style.marginTop = originalOffsetTop+"px";
   }
 }
