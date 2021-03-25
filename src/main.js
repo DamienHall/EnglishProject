1b69f29349770e7ad49dcbffaffddcdc46141650
LilZcrazyG <damien.z.hall@gmail.com>
2021-03-25 14:57:20 -0500

    Sticky Nav Bar Changes, hopefully this finally works well ;-;

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
+var content = document.getElementById("content");
+var originalMarginTop = content.style.marginTop;
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
 window.onscroll = () => {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky");
+    content.style.marginTop = (content.style.marginTop+(navbar.clientHeight*2))+"px";
   } else {
     navbar.classList.remove("sticky");
+    content.style.marginTop = originalMarginTop;
   }
 }
